bd54bf270ffbaac5cfe3c5d2629926feeb5f0365
bigtreetech <tech@biqu3d.com>
2024-11-29 09:48:47 +0800

    boot: add bigtreetech scripts

---
+++ patch/boot/scripts/vibration.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#================================
+# enable `pad7_vibration` overlays
+#================================
+
+echo 1 > /sys/class/leds/\:pad7_vibration/brightness
+sleep 0.04
+echo 0 > /sys/class/leds/\:pad7_vibration/brightness
