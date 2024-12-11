93da41320cea1681327fbb3f7a8a427ce1eb517a
bigtreetech <tech@biqu3d.com>
2024-12-11 17:16:13 +0800

    boot: clean up copy gcodes shell

--- patch/boot/scripts/btt_init.sh
+++ patch/boot/scripts/btt_init.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-sudo chown biqu:biqu /home/biqu/ -R
-# sudo ntpdate stdtime.gov.hk
-
-cd /boot/gcode
-if ls *.gcode > /dev/null 2>&1;then
-    sudo cp ./*.gcode /home/biqu/printer_data/gcodes -fr
-    sudo rm ./*.gcode -fr
-fi
-sync
-
 cd /boot/scripts
 
 #./extend_fs.sh &
+
+./copy_gcode.sh &
 
 ./system_cfg.sh &
 
