2e91cc9e145960eccbc6b5d584793e87daff0894
Alan.Ma <tech@biqu3d.com>
2025-04-27 15:17:53 +0800

    user boot: use armbian-resize-filesystem instead of resize2fs self

--- patch/boot/scripts/extend_fs.sh
+++ patch/boot/scripts/extend_fs.sh
@@ -1,35 +1,5 @@
 #!/bin/bash
 
-c=0
-cd /dev
-for file in `ls mmcblk*`
-do
-    filelist[$c]=$file
-    ((c++))
-done
-
-ROOT_DEV=/dev/${filelist[0]}
-
-BOOT_NUM=1
-ROOT_NUM=2
-
-ROOTFS_START=532480
-
-# fdisk
-sudo fdisk "$ROOT_DEV" << EOF
-p
-d
-$ROOT_NUM
-n
-p
-$ROOT_NUM
-$ROOTFS_START
-
-y
-w
-EOF
-
-sudo resize2fs /dev/${filelist[0]}p${ROOT_NUM}
-unset filelist
+sudo /usr/lib/armbian/armbian-resize-filesystem start
 
 sudo sed -i '/^.\/extend_fs.sh/s/^/#/' /boot/scripts/btt_init.sh
