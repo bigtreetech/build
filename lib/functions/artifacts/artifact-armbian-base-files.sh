cc4a969e49a9bb49cc7d383c3b1cd3dcba2326dd
Alan.Ma <tech@biqu3d.com>
2026-03-20 11:14:55 +0800

    base_files version must not be less then 13.3 for the trixie debian13 rule check

--- lib/functions/artifacts/artifact-armbian-base-files.sh
+++ lib/functions/artifacts/artifact-armbian-base-files.sh
@@ -61,7 +61,7 @@ function artifact_armbian-base-files_prepare_version() {
 
 	# Important. Force the final reversioned version to contain the release name.
 	# Otherwise, when publishing to a repo, pool/main/b/base-files/base-files_${REVISION}.deb will be the same across releases.
-	artifact_final_version_reversioned="${REVISION}-${base_files_wanted_upstream_version}-${RELEASE}"
+	artifact_final_version_reversioned="13.3""${REVISION}-${base_files_wanted_upstream_version}-${RELEASE}"
 
 	# Register the function used to re-version the _contents_ of the base-files deb file.
 	artifact_debs_reversion_functions+=("reversion_armbian-base-files_deb_contents")
